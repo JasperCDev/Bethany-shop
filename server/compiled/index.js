026308ccacfdfc710f4105756df1123b94fca4de
JasperC1999 <jasperischauvin@gmail.com>
2020-07-25 17:21:25 -0500

    init typescript server

--- server/compiled/index.js
+++ server/compiled/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log('test');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+var PORT = process.env.PORT || 5000;
+app.get('/', function (req, res) {
+    res.send('typescript-node');
+});
+app.listen(PORT, function () { return console.log("Server listening on port " + PORT); });
--- server/typescript/index.ts
+++ server/typescript/index.ts
@@ -1,1 +1,11 @@
-console.log('test');
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('typescript-node');
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));

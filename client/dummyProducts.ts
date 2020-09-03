a67950c7eb48bdfa18dda499f3858eff1033f04c
JasperC1999 <jasperischauvin@gmail.com>
2020-09-02 20:59:52 -0500

    added all new pictures

--- client/dummyProducts.ts
+++ client/dummyProducts.ts
@@ -2,7 +2,7 @@ export const dummyProducts = [
   {
     id: 1,
     imageMainUrl: 'https://i.imgur.com/Dqn4pIZ.jpeg',
-    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg'],
+    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/MJrrzuB.jpeg', 'https://i.imgur.com/AGg1WMC.jpeg', 'https://i.imgur.com/SPsHvOc.jpeg', 'https://i.imgur.com/Lc00Tqa.jpeg', 'https://i.imgur.com/VpnKhOJ.jpeg'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, libero at facilisis consectetur, nulla ante vehicula urna, quis consectetur lacus nunc ut ante. Etiam diam tortor, molestie eu metus vel, porttitor ultricies tellus. Aliquam pretium velit eget venenatis iaculis. Duis scelerisque, lacus vel ornare pulvinar, justo eros finibus justo, sit amet rhoncus leo orci vitae nunc.',
     price: 39.99,
     title: 'blue lululemon pants',
@@ -13,8 +13,8 @@ export const dummyProducts = [
   },
   {
     id: 2,
-    imageMainUrl: 'https://i.imgur.com/Dqn4pIZ.jpeg',
-    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg'],
+    imageMainUrl: 'https://i.imgur.com/LzKCwLX.jpeg',
+    images: ['https://i.imgur.com/LzKCwLX.jpeg', 'https://i.imgur.com/ZpwcL78.jpeg', 'https://i.imgur.com/OcutGAG.jpeg', 'https://i.imgur.com/cD2rEwf.jpeg', 'https://i.imgur.com/2jZVh1e.jpeg'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, libero at facilisis consectetur, nulla ante vehicula urna, quis consectetur lacus nunc ut ante. Etiam diam tortor, molestie eu metus vel, porttitor ultricies tellus. Aliquam pretium velit eget venenatis iaculis. Duis scelerisque, lacus vel ornare pulvinar, justo eros finibus justo, sit amet rhoncus leo orci vitae nunc.',
     price: 39.99,
     title: 'blue lululemon pants',
@@ -25,8 +25,8 @@ export const dummyProducts = [
   },
   {
     id: 3,
-    imageMainUrl: 'https://i.imgur.com/Dqn4pIZ.jpeg',
-    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg'],
+    imageMainUrl: 'https://i.imgur.com/LWGutQG.jpeg',
+    images: ['https://i.imgur.com/LWGutQG.jpeg', 'https://i.imgur.com/6ssYYjq.jpeg', 'https://i.imgur.com/EgkEym6.jpeg', 'https://i.imgur.com/mbUKosQ.jpeg', 'https://i.imgur.com/J40dvaG.jpeg'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, libero at facilisis consectetur, nulla ante vehicula urna, quis consectetur lacus nunc ut ante. Etiam diam tortor, molestie eu metus vel, porttitor ultricies tellus. Aliquam pretium velit eget venenatis iaculis. Duis scelerisque, lacus vel ornare pulvinar, justo eros finibus justo, sit amet rhoncus leo orci vitae nunc.',
     price: 39.99,
     title: 'blue lululemon pants',
@@ -37,8 +37,8 @@ export const dummyProducts = [
   },
   {
     id: 4,
-    imageMainUrl: 'https://i.imgur.com/Dqn4pIZ.jpeg',
-    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg'],
+    imageMainUrl: 'https://i.imgur.com/lvaGJWN.jpeg',
+    images: ['https://i.imgur.com/lvaGJWN.jpeg', 'https://i.imgur.com/jvVkTOD.jpeg', 'https://i.imgur.com/1q4Kmnj.jpeg', 'https://i.imgur.com/60zlSz5.jpeg', 'https://i.imgur.com/9QNF0lt.jpeg', 'https://i.imgur.com/qPFKlKg.jpeg'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, libero at facilisis consectetur, nulla ante vehicula urna, quis consectetur lacus nunc ut ante. Etiam diam tortor, molestie eu metus vel, porttitor ultricies tellus. Aliquam pretium velit eget venenatis iaculis. Duis scelerisque, lacus vel ornare pulvinar, justo eros finibus justo, sit amet rhoncus leo orci vitae nunc.',
     price: 39.99,
     title: 'blue lululemon pants',
@@ -49,8 +49,8 @@ export const dummyProducts = [
   },
   {
     id: 5,
-    imageMainUrl: 'https://i.imgur.com/Dqn4pIZ.jpeg',
-    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg'],
+    imageMainUrl: 'https://i.imgur.com/Bn1x0mv.jpg',
+    images: ['https://i.imgur.com/Bn1x0mv.jpg', 'https://i.imgur.com/4hAeWjS.jpg', 'https://i.imgur.com/nS4pQ7F.jpg', 'https://i.imgur.com/Cz8Df1J.jpg', 'https://i.imgur.com/TRd0Fvi.jpg', 'https://i.imgur.com/jMvOkmv.jpg'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, libero at facilisis consectetur, nulla ante vehicula urna, quis consectetur lacus nunc ut ante. Etiam diam tortor, molestie eu metus vel, porttitor ultricies tellus. Aliquam pretium velit eget venenatis iaculis. Duis scelerisque, lacus vel ornare pulvinar, justo eros finibus justo, sit amet rhoncus leo orci vitae nunc.',
     price: 39.99,
     title: 'blue lululemon pants',
@@ -61,8 +61,8 @@ export const dummyProducts = [
   },
   {
     id: 6,
-    imageMainUrl: 'https://i.imgur.com/Dqn4pIZ.jpeg',
-    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg'],
+    imageMainUrl: 'https://i.imgur.com/E5aqlX3.jpg',
+    images: ['https://i.imgur.com/E5aqlX3.jpg', 'https://i.imgur.com/GnWrERN.jpg', 'https://i.imgur.com/w1qLIEl.jpg', 'https://i.imgur.com/ubhGolp.jpg', 'https://i.imgur.com/3eVw0uf.jpg', 'https://i.imgur.com/zEjVp0J.jpg'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, libero at facilisis consectetur, nulla ante vehicula urna, quis consectetur lacus nunc ut ante. Etiam diam tortor, molestie eu metus vel, porttitor ultricies tellus. Aliquam pretium velit eget venenatis iaculis. Duis scelerisque, lacus vel ornare pulvinar, justo eros finibus justo, sit amet rhoncus leo orci vitae nunc.',
     price: 39.99,
     title: 'blue lululemon pants',

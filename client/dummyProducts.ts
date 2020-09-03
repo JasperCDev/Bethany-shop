948467400503259fc5417180535c5f09f50c7be6
JasperC1999 <jasperischauvin@gmail.com>
2020-09-02 20:29:20 -0500

    added bethany's pic to site

--- client/dummyProducts.ts
+++ client/dummyProducts.ts
@@ -1,8 +1,8 @@
 export const dummyProducts = [
   {
     id: 1,
-    imageMainUrl: 'https://i.pinimg.com/originals/be/8d/2b/be8d2b8e9a1563780a22103cb1c658f5.jpg',
-    images: ['https://i.pinimg.com/originals/be/8d/2b/be8d2b8e9a1563780a22103cb1c658f5.jpg', 'https://images.lululemon.com/is/image/lululemon/LW5CQOS_044752_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW8A81R_035167_3?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72'],
+    imageMainUrl: 'https://i.imgur.com/Dqn4pIZ.jpeg',
+    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, libero at facilisis consectetur, nulla ante vehicula urna, quis consectetur lacus nunc ut ante. Etiam diam tortor, molestie eu metus vel, porttitor ultricies tellus. Aliquam pretium velit eget venenatis iaculis. Duis scelerisque, lacus vel ornare pulvinar, justo eros finibus justo, sit amet rhoncus leo orci vitae nunc.',
     price: 39.99,
     title: 'blue lululemon pants',
@@ -13,8 +13,8 @@ export const dummyProducts = [
   },
   {
     id: 2,
-    imageMainUrl: 'https://images.lululemon.com/is/image/lululemon/LW1CF4S_046693_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
-    images: ['https://images.lululemon.com/is/image/lululemon/LW1CF4S_046693_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW5CQOS_044752_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW8A81R_035167_3?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://i.pinimg.com/originals/be/8d/2b/be8d2b8e9a1563780a22103cb1c658f5.jpg', 'https://images.lululemon.com/is/image/lululemon/LW2BN4S_034268_1?$image_carousel_zoom-modal$&wid=1800&op_usm=0.8,1,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72'],
+    imageMainUrl: 'https://i.imgur.com/Dqn4pIZ.jpeg',
+    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, libero at facilisis consectetur, nulla ante vehicula urna, quis consectetur lacus nunc ut ante. Etiam diam tortor, molestie eu metus vel, porttitor ultricies tellus. Aliquam pretium velit eget venenatis iaculis. Duis scelerisque, lacus vel ornare pulvinar, justo eros finibus justo, sit amet rhoncus leo orci vitae nunc.',
     price: 39.99,
     title: 'blue lululemon pants',
@@ -25,8 +25,8 @@ export const dummyProducts = [
   },
   {
     id: 3,
-    imageMainUrl: 'https://images.lululemon.com/is/image/lululemon/LW5CQOS_044752_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
-    images: ['https://images.lululemon.com/is/image/lululemon/LW5CQOS_044752_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW1CF4S_046693_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW8A81R_035167_3?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW2BN4S_034268_1?$image_carousel_zoom-modal$&wid=1800&op_usm=0.8,1,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW1BVOS_043177_5?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72'],
+    imageMainUrl: 'https://i.imgur.com/Dqn4pIZ.jpeg',
+    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, libero at facilisis consectetur, nulla ante vehicula urna, quis consectetur lacus nunc ut ante. Etiam diam tortor, molestie eu metus vel, porttitor ultricies tellus. Aliquam pretium velit eget venenatis iaculis. Duis scelerisque, lacus vel ornare pulvinar, justo eros finibus justo, sit amet rhoncus leo orci vitae nunc.',
     price: 39.99,
     title: 'blue lululemon pants',
@@ -37,8 +37,8 @@ export const dummyProducts = [
   },
   {
     id: 4,
-    imageMainUrl: 'https://images.lululemon.com/is/image/lululemon/LW8A81R_035167_3?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
-    images: ['https://images.lululemon.com/is/image/lululemon/LW8A81R_035167_3?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW1CF4S_046693_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW5CQOS_044752_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://i.pinimg.com/originals/be/8d/2b/be8d2b8e9a1563780a22103cb1c658f5.jpg', 'https://images.lululemon.com/is/image/lululemon/LW2BN4S_034268_1?$image_carousel_zoom-modal$&wid=1800&op_usm=0.8,1,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72'],
+    imageMainUrl: 'https://i.imgur.com/Dqn4pIZ.jpeg',
+    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, libero at facilisis consectetur, nulla ante vehicula urna, quis consectetur lacus nunc ut ante. Etiam diam tortor, molestie eu metus vel, porttitor ultricies tellus. Aliquam pretium velit eget venenatis iaculis. Duis scelerisque, lacus vel ornare pulvinar, justo eros finibus justo, sit amet rhoncus leo orci vitae nunc.',
     price: 39.99,
     title: 'blue lululemon pants',
@@ -49,8 +49,8 @@ export const dummyProducts = [
   },
   {
     id: 5,
-    imageMainUrl: 'https://images.lululemon.com/is/image/lululemon/LW1BVOS_043177_5?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
-    images: ['https://images.lululemon.com/is/image/lululemon/LW1BVOS_043177_5?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW1CF4S_046693_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW5CQOS_044752_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW8A81R_035167_3?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW2BN4S_034268_1?$image_carousel_zoom-modal$&wid=1800&op_usm=0.8,1,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72'],
+    imageMainUrl: 'https://i.imgur.com/Dqn4pIZ.jpeg',
+    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, libero at facilisis consectetur, nulla ante vehicula urna, quis consectetur lacus nunc ut ante. Etiam diam tortor, molestie eu metus vel, porttitor ultricies tellus. Aliquam pretium velit eget venenatis iaculis. Duis scelerisque, lacus vel ornare pulvinar, justo eros finibus justo, sit amet rhoncus leo orci vitae nunc.',
     price: 39.99,
     title: 'blue lululemon pants',
@@ -61,8 +61,8 @@ export const dummyProducts = [
   },
   {
     id: 6,
-    imageMainUrl: 'https://images.lululemon.com/is/image/lululemon/LW2BN4S_034268_1?$image_carousel_zoom-modal$&wid=1800&op_usm=0.8,1,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72',
-    images: ['https://images.lululemon.com/is/image/lululemon/LW2BN4S_034268_1?$image_carousel_zoom-modal$&wid=1800&op_usm=0.8,1,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW1CF4S_046693_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW5CQOS_044752_1?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://images.lululemon.com/is/image/lululemon/LW8A81R_035167_3?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 'https://i.pinimg.com/originals/be/8d/2b/be8d2b8e9a1563780a22103cb1c658f5.jpg', 'https://images.lululemon.com/is/image/lululemon/LW1BVOS_043177_5?$pdp_image_carousel$&wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=90,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72'],
+    imageMainUrl: 'https://i.imgur.com/Dqn4pIZ.jpeg',
+    images: ['https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg', 'https://i.imgur.com/Dqn4pIZ.jpeg'],
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, libero at facilisis consectetur, nulla ante vehicula urna, quis consectetur lacus nunc ut ante. Etiam diam tortor, molestie eu metus vel, porttitor ultricies tellus. Aliquam pretium velit eget venenatis iaculis. Duis scelerisque, lacus vel ornare pulvinar, justo eros finibus justo, sit amet rhoncus leo orci vitae nunc.',
     price: 39.99,
     title: 'blue lululemon pants',
